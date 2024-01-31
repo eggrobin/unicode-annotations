3b9219af4ef4d3ffcebdbe5c79e9e0e79169fc31
Robin Leroy <egg.robin.leroy@gmail.com>
2024-01-31 01:14:22 +0100

    an annotation for 16.0

--- annotations.py
+++ annotations.py
@@ -1039,4 +1039,11 @@ ISSUES = (
           paragraphs=[
               ParagraphNumber(324, 10),
           ]),
+    # lb=PR defaults in the Currency Symbols block, undocumented for ages.
+    Issue(Version(16, 0, 0),
+          ["133-C26"],
+          paragraphs=[
+              ParagraphNumber(312),
+              ParagraphNumber(596, 77),
+          ]),
 )
