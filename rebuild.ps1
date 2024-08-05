4da95f651d8083934a0d007fdb95eb061b0727f2
Robin Leroy <egg.robin.leroy@gmail.com>
2024-08-05 15:50:10 +0200

    Update rebuild.ps1 to something that will only start working in September.

--- rebuild.ps1
+++ rebuild.ps1
@@ -1,7 +1,7 @@
 foreach (
     $i in @(6, 7, 10, 12, 14, 15, 17, 19, 22, 24, 26, 28,
             30, 32, 33, 35, 37, 39, 41, 43, 45, 47, 49,
-            51, 52)) {
+            51, 53)) {
   Invoke-WebRequest "https://www.unicode.org/reports/tr14/tr14-$i.html" -OutFile "tr14-$i.html"
 }
 python .\lb_rule_extractor.py --out=paragraphs.py
